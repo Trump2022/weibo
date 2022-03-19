5d1f33941037f2db2bbccfd147f715ea98a2fa90
trump2022 <qsw2012@outlook.com>
2022-03-19 20:11:11 +0800

    发送找回密码链接

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
 
 class PasswordController extends Controller
 {
